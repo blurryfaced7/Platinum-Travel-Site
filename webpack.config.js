731dc9c5e453322bff1479b262cfe253080e3cc7
Pyoyoyo <sodbileg.d13@gmail.com>
2023-06-27 21:29:57 +0800

    47r hicheel hurtel hugjuulelt hiigdsen.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const postcssPlugins = [
+    require("postcss-import"), 
+    require("postcss-mixins"),
+    require("postcss-simple-vars"), 
+    require("postcss-nested"), 
+    require("autoprefixer")
+];
+
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        watchFiles: ('./app/**/*.html'),
+        static: {
+            directory: path.join(__dirname, "./app")
+          },
+        hot: true,
+        host: '0.0.0.0',
+        port: 3000
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", {loader: "postcss-loader", 
+                options: {postcssOptions: {plugins: postcssPlugins}}}],
+            }
+        ]
+    }
+}
+
+
