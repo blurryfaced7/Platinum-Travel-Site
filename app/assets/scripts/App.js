d1d0ea2c41e3a1f9be3b60d9add532e1e02d98fe
Pyoyoyo <sodbileg.d13@gmail.com>
2023-07-05 16:48:27 +0800

    Mobile Tsesiig hiij duusgaw

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,23 +1,12 @@
 import "../styles/styles.css";
-import Person from "./modules/Person";
+import MobileMenu from "./modules/MobileMenu";
 
-if(module.hot){
+let mobileMenu = new MobileMenu();
+
+if (module.hot) {
+
     module.hot.accept();
 }
 
 
-// Ene muruus doosh jishee code-uud baigaa
 
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " odoogoor tatwariin urgui bn.");
-    }
-}
-
-var john = new Person("John Doe", "green");
-john.greet();
-// john.payTaxes();
-
-var jane = new Adult("Jane Doe", "pink");
-jane.greet();
-jane.payTaxes();
