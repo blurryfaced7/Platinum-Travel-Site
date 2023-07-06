3827f73e0c6732b98c43657e71a60156ff301e4a
Pyoyoyo <sodbileg.d13@gmail.com>
2023-07-06 18:47:10 +0800

    Cloud function-iig update hiilee.

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,30 @@
-exports.handler = function(events, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Hereglegchdiin huudsand tavtai morilno uu"
-    })
+exports.handler = function(event, context, callback) {
+
+    const secretContent = `
+    <h3> Hereglegchiin tusgai huudsand tavtai morilno uu!</h3>
+    <p>Zuvhun durtgeltei hereglegchded zoriulsan <strong>haalttai contentuud</strong></p>
+    ` 
+    let body;
+
+    if(event.body) {
+        body = JSON.parse(event.body);
+
+    }
+    else {
+        body = {}
+    }
+
+    if(body.password == "JavaScript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    }
+    else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+
+    
 }
