faca11dffb8b4f9432813446b1831d0d563c9462
Pyoyoyo <sodbileg.d13@gmail.com>
2023-07-06 14:26:36 +0800

    modal overlayiig buren hiij duuslaa.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,32 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+// import Modal from "./modules/Modal";
 
 
-let stickyHeader = new StickyHeader();
+// new Modal();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
+new MobileMenu();
+let modal; 
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => 
+    {el.addEventListener("click", e => {
+        e.preventDefault();
+        if(typeof modal == "undefined") {
+            import(/*webpackChunkName: "modal"*/ "./modules/Modal")
+                .then(x => {
+                    modal = new x.default();
+                    setTimeout(() => modal.openModal(), 20);
+                })
+            .catch(() => console.log("file-g duudahad aldaa garlaa"));
+        }
+        else{
+            modal.openModal();
+        }
+    })
+});
 
 if (module.hot) {
 
